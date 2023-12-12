5bbdd90c4837ed2f6eb5168c57c8d9b327f85490
prashant <coolrai39@gmail.com>
2023-12-12 22:05:35 +0530

    some more changes

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import operator
-l = [1,2,3,4,5]
-x = operator.itemgetter(l[2])
-print(x)
+list1 = [1,9,3,2,7,4,8]
+
+for i in range(len(list1)):
+    for j in range((i+1),len(list1)):
+        if list1[i] < list1[j]:
+            list1[i],list1[j]=list1[j],list1[i]
+print(list1)
